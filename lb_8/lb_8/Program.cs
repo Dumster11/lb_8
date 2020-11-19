e83ce38ff2af021129fc99f5ebc1004a0bab2a78
Dumster11 <71654055+Dumster11@users.noreply.github.com>
2020-11-19 12:39:09 +0200

    ss

---
+++ lb_8/lb_8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lb_8_1
+{
+    class MyClass
+    {
+        private int[] myArray = new int[] { 1, 2, 3 };
+        private string x = "x";
+        public int this[int index]
+        {
+            set { myArray[index] = value; }
+            get { return myArray[index]; }
+        }
+        public string this[string i]
+        {
+            get { return x; }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            instance[2] = 1;
+            for (int i = 0; i < 3; i++) ;
+            {
+                Console.WriteLine(instance["index"]);
+            }
+            Console.WriteLine(instance["index2"]);
+            Console.ReadKey();
+        }
+    }
+}
